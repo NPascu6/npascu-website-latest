fa4b2c7d3e5e843c25d457bb9f780eada2a4ea02
Norbert Pascu <norbert.pascu@juliusbaer.com>
2024-05-30 14:32:28 +0200

    Updated text.

--- src/components/main-page/HighlightedRepos.tsx
+++ src/components/main-page/HighlightedRepos.tsx
@@ -1,51 +1,72 @@
 const HighlightedRepos = () => {
-    return <div className="m-2 mx-4 p-4 text-start card">
-        <p>
-            <a
-                style={{ textDecoration: 'underline' }}
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://github.com/NPascu6/npascu-website-latest"
-                className={` font-bold`}
-            >
-                Pascu.io
-            </a>
-            <span>
-                - Created with React 18, Tailwind Css, Redux, React Router, and deployed to my personal GoDaddy domain, with github actions and FTP Deploy.
-            </span>
-        </p>
-        <hr className="p-2 border-b-1" />
-        <p>
-            <a
-                style={{ textDecoration: 'underline' }}
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://github.com/NPascu6/npascu-api-v1"
-                className={`font-bold`}
-            >
-                Personal API
-            </a>
-            <span>
-                - Created with .NET Core 7.0, Entity Framework Core, SQL Server, Swagger, JWT Authentication, and deployed to my personal azure container and application platform, which you can access by clicking:
-                <span className="font-bold ml-2 cursor-pointer" style={{ textDecoration: 'underline' }} onClick={() => window.open("https://norbert-pascu.com", "_blank")}>npascu-api-v1.azurewebsites.net</span>
-            </span>
-        </p>
-        <hr className="p-2 border-b-1" />
-        <p>
-            <a
-                style={{ textDecoration: 'underline' }}
-                rel="noopener noreferrer"
-                target="_blank"
-                className={`font-bold`}
-                href="https://github.com/NPascu6/andrei-mylenses.ch">
-                Andrei-Mylenses
-            </a>
-            <span>
-                - Created with React 18, Tailwind Css, Redux, React Router, and deployed to my personal GoDaddy domain, with github actions and FTP Deploy, which you can access my clicking:
-                <span className="font-bold ml-2 cursor-pointer" style={{ textDecoration: 'underline' }} onClick={() => window.open("https://andrei-mylenses.ch", "_blank")}>andrei-mylenses.ch</span>
-            </span>
-        </p>
+  return (
+    <div className="m-2 mx-4 p-4 text-start card">
+      <p>
+        <a
+          style={{ textDecoration: "underline" }}
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/NPascu6/npascu-website-latest"
+          className={` font-bold`}
+        >
+          Pascu.io
+        </a>
+        <span>
+          - Created with React 18, Tailwind Css, Redux, React Router, and
+          deployed to my personal GoDaddy domain, with github actions and FTP
+          Deploy.
+        </span>
+      </p>
+      <hr className="p-2 border-b-1" />
+      <p>
+        <a
+          style={{ textDecoration: "underline" }}
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/NPascu6/npascu-api-v1"
+          className={`font-bold`}
+        >
+          Personal API
+        </a>
+        <span>
+          - Created with .NET Core 7.0, Entity Framework Core, SQL Server,
+          Swagger, JWT Authentication, and deployed to my personal azure
+          container and application platform, which you can access by clicking:
+          <span
+            className="font-bold ml-2 cursor-pointer"
+            style={{ textDecoration: "underline" }}
+            onClick={() => window.open("https://norbert-pascu.com", "_blank")}
+          >
+            https://norbert-pascu.com
+          </span>
+        </span>
+      </p>
+      <hr className="p-2 border-b-1" />
+      <p>
+        <a
+          style={{ textDecoration: "underline" }}
+          rel="noopener noreferrer"
+          target="_blank"
+          className={`font-bold`}
+          href="https://github.com/NPascu6/andrei-mylenses.ch"
+        >
+          Andrei-Mylenses
+        </a>
+        <span>
+          - Created with React 18, Tailwind Css, Redux, React Router, and
+          deployed to my personal GoDaddy domain, with github actions and FTP
+          Deploy, which you can access my clicking:
+          <span
+            className="font-bold ml-2 cursor-pointer"
+            style={{ textDecoration: "underline" }}
+            onClick={() => window.open("https://andrei-mylenses.ch", "_blank")}
+          >
+            andrei-mylenses.ch
+          </span>
+        </span>
+      </p>
     </div>
-}
+  );
+};
 
 export default HighlightedRepos;
