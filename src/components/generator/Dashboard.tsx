c4c61b5b615122d93b0c1111304eb585cca3dc6b
Norbert Pascu <norbert.pascu@mavx.com>
2024-11-11 15:56:04 +0100

    Small fix

--- src/components/generator/Dashboard.tsx
+++ src/components/generator/Dashboard.tsx
@@ -1,14 +1,15 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DynamicTable from "./DynamicTable";
-import {tables} from "../../assets/tableDb";
-import {formDb} from "../../assets/formDb";
+import { tables } from "../../assets/tableDb";
+import { formDb } from "../../assets/formDb";
 import FormDetail from "./FormDetail";
 import CollapsibleSection from "../common/CollapsableSection";
+import CloseIcon from "../../assets/icons/CloseIcon";
 
 const FormList: React.FC = () => {
     const [loading,] = useState(false);
     const [useTabs, setUseTabs] = useState<boolean>(false);
-    const [selectedTableId, setSelectedTableId] = useState<number>(8);
+    const [selectedTableId, setSelectedTableId] = useState<number>();
     const [activeForm, setActiveForm] = useState<number>(0);
 
     const handleTabClick = (tableId: number) => {
@@ -25,7 +26,7 @@ const FormList: React.FC = () => {
 
     return (
         <div className="flex flex-col md:flex-row justify-center items-center w-full">
-            <div className="w-full">
+            {!selectedTableId && <div className="w-full">
                 <CollapsibleSection title="Dynamic Forms">
                     <div className="w-full p-1 shadow-md rounded-lg">
                         <h1 className="text-md font-bold mb-2">Select a Form</h1>
@@ -39,33 +40,42 @@ const FormList: React.FC = () => {
                                     >
                                         <span className="text-xs">{form.id}. </span>
                                         <span className="text-xs text-indigo-600 hover:underline">
-                      {form.form_name}
-                    </span>
+                                            {form.form_name}
+                                        </span>
                                     </li>
                                 ))}
                         </ul>
                         {loading && <p className="mt-4 text-gray-600">Loading...</p>}
                     </div>
                 </CollapsibleSection>
-            </div>
+            </div>}
 
             <div className="w-full mt-1 md:mt-0">
                 {activeForm !== 0 ? (
-                    <div style={{height: "calc(100dvh - 13em)", overflow: "auto"}}>
-                        <FormDetail id={activeForm} setActiveForm={setActiveForm}/>
+                    <div style={{ height: "calc(100dvh - 13em)", overflow: "auto" }}>
+                        <FormDetail id={activeForm} setActiveForm={setActiveForm} />
                     </div>
                 ) : (
                     <CollapsibleSection title="Dynamic Table Data">
                         {activeForm === 0 && (
                             <div className="flex flex-col md:flex-row justify-between items-center w-full mb-1">
-                                <h1 className="text-lg font-semibold mr-2">
+                                <h1 className="text-lg font-semibold mr-2 ml-2">
                                     Dynamic Tables with AG Grid
                                 </h1>
                                 <button
                                     onClick={toggleView}
-                                    className="px-1 text-sm font-semibold rounded-md shadow-md"
+                                    className="px-1 text-sm font-semibold rounded-md shadow-md mr-2"
                                 >
                                     {useTabs ? "Switch to Dropdown" : "Switch to Tabs"}
+                                </button>
+                                <button
+                                    style={{ alignSelf: "flex-end", marginBottom: "4em" }}
+                                    type="button"
+                                    className="font-semibold rounded-md hover:bg-gray-400 mr-2
+                                    focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                                    onClick={() => setActiveForm(0)}
+                                >
+                                    <CloseIcon />
                                 </button>
                             </div>
                         )}
@@ -75,16 +85,15 @@ const FormList: React.FC = () => {
                                     <div className="border-b border-gray-200 mb-2">
                                         <ul
                                             className="flex space-x-2 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100"
-                                            style={{maxWidth: "100%"}}
+                                            style={{ maxWidth: "100%" }}
                                         >
                                             {tables.map((table) => (
                                                 <li
                                                     key={table.id}
-                                                    className={`cursor-pointer px-2 py-2 whitespace-nowrap ${
-                                                        selectedTableId === table.id
-                                                            ? "text-black font-bold bg-gray-400"
-                                                            : "text-gray-500 hover:text-blue-500"
-                                                    }`}
+                                                    className={`cursor-pointer px-2 py-2 whitespace-nowrap ${selectedTableId === table.id
+                                                        ? "text-black font-bold bg-gray-400"
+                                                        : "text-gray-500 hover:text-blue-500"
+                                                        }`}
                                                     onClick={() => handleTabClick(table.id)}
                                                 >
                                                     {table.table_name}
@@ -108,7 +117,8 @@ const FormList: React.FC = () => {
                                 )}
                             </>
                         )}
-                        <DynamicTable tableId={selectedTableId}/>
+                        {selectedTableId && <DynamicTable tableId={selectedTableId} />}
+
                     </CollapsibleSection>
                 )}
             </div>
--- src/components/main-page/GamesContainer.tsx
+++ src/components/main-page/GamesContainer.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import {Link, Route, Routes} from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import GamePage from "../../pages/game/TicTacToeGame";
 import SnakeGame from "../../pages/game/SnakeGame";
 import SnakeIcon from "../../assets/icons/Snake";
 import TicTacToeIcon from "../../assets/icons/TicTacToe";
 import ClickTheTargetIcon from "../../assets/icons/ClickTheTargetIcon";
 import ClickTheTarget from "../../pages/game/ClickTheTarget";
-import {useSelector} from "react-redux";
-import {RootState} from "../../store/store";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import Dashboard from "../generator/Dashboard";
-// import BrickOutGame from './BrickoutGame';
 
 const GameCard = () => {
     const [isGamesActive, setIsGamesActive] = React.useState(true);
@@ -22,37 +21,31 @@ const GameCard = () => {
         <div className="game-card shadow-xl">
             <div className="flex items-center justify-center">
                 <div
-                    className={`cursor-pointer px-4 py-2 border-b-2 ${
-                        !isGamesActive
-                            ? `${
-                                isDarkTheme
-                                    ? "border-transparent bg-gray-800 text-white"
-                                    : "border-transparent bg-white text-black"
-                            } font-bold opacity-50`
-                            : `${
-                                isDarkTheme
-                                    ? "border-gray-600 bg-gray-700 text-white"
-                                    : "border-gray-300 bg-gray-100 text-black"
-                            } font-semibold opacity-100`
-                    }`}
+                    className={`cursor-pointer px-4 py-2 border-b-2 ${!isGamesActive
+                        ? `${isDarkTheme
+                            ? "border-transparent bg-gray-800 text-white"
+                            : "border-transparent bg-white text-black"
+                        } font-bold opacity-50`
+                        : `${isDarkTheme
+                            ? "border-gray-600 bg-gray-700 text-white"
+                            : "border-gray-300 bg-gray-100 text-black"
+                        } font-semibold opacity-100`
+                        }`}
                     onClick={() => handleTabClick()}
                 >
                     Browser Games
                 </div>
                 <div
-                    className={`cursor-pointer px-2 py-2 border-b-2 ${
-                        isGamesActive
-                            ? `${
-                                isDarkTheme
-                                    ? "border-transparent bg-gray-800 text-white"
-                                    : "border-transparent bg-white text-black"
-                            } font-bold opacity-50`
-                            : `${
-                                isDarkTheme
-                                    ? "border-gray-600 bg-gray-700 text-white"
-                                    : "border-gray-300 bg-gray-100 text-black"
-                            } font-semibold opacity-100`
-                    }`}
+                    className={`cursor-pointer px-2 py-2 border-b-2 ${isGamesActive
+                        ? `${isDarkTheme
+                            ? "border-transparent bg-gray-800 text-white"
+                            : "border-transparent bg-white text-black"
+                        } font-bold opacity-50`
+                        : `${isDarkTheme
+                            ? "border-gray-600 bg-gray-700 text-white"
+                            : "border-gray-300 bg-gray-100 text-black"
+                        } font-semibold opacity-100`
+                        }`}
                     onClick={() => handleTabClick()}
                 >
                     Dynamic Components
@@ -68,7 +61,7 @@ const GameCard = () => {
                             <div className="flex align-center justify-between items-center">
                                 <h2 className="">Tic Tac Toe</h2>
                                 <div className="game-icons">
-                                    <TicTacToeIcon/>
+                                    <TicTacToeIcon />
                                 </div>
                             </div>
                         </Link>
@@ -81,7 +74,7 @@ const GameCard = () => {
                             <div className=" flex align-center justify-between items-center">
                                 <h2>Snake</h2>
                                 <div className="game-icons">
-                                    <SnakeIcon/>
+                                    <SnakeIcon />
                                 </div>
                             </div>
                         </Link>
@@ -94,7 +87,7 @@ const GameCard = () => {
                             <div className=" flex align-center justify-between items-center">
                                 <h2>Click The Target</h2>
                                 <div className="game-icons">
-                                    <ClickTheTargetIcon/>
+                                    <ClickTheTargetIcon />
                                 </div>
                             </div>
                         </Link>
@@ -102,7 +95,7 @@ const GameCard = () => {
                 </div>
             ) : (
                 <div className="p-1 flex md:flex-col text-center items-center align-center ">
-                    <Dashboard/>
+                    <Dashboard />
                 </div>
             )}
         </div>
@@ -113,14 +106,10 @@ const GamesCardPage = () => {
     return (
         <div className="">
             <Routes>
-                {/* Parent route renders GamesCardPage component */}
-                <Route path="*" element={<GameCard/>}/>
-                {/* Child routes */}
-                <Route path="tic-tac-toe" element={<GamePage/>}/>
-                <Route path="snake" element={<SnakeGame/>}/>
-                <Route path="click-the-target" element={<ClickTheTarget/>}/>
-                {/* <Route path="brick-out" element={<BrickOutGame />} /> */}
-                {/* Add other nested routes as needed */}
+                <Route path="*" element={<GameCard />} />
+                <Route path="tic-tac-toe" element={<GamePage />} />
+                <Route path="snake" element={<SnakeGame />} />
+                <Route path="click-the-target" element={<ClickTheTarget />} />
             </Routes>
         </div>
     );
