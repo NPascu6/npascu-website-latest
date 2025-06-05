f6319223db40942c46c4b2a56f152d941587ddab
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2025-06-05 13:23:28 +0200

    Fix style object syntax in click target game

--- src/components/games/click-the-target/Game.tsx
+++ src/components/games/click-the-target/Game.tsx
@@ -211,7 +211,8 @@ const Game = ({setStarted}: GameProps) => {
                                 left: `${target.position.x}px`,
                                 top: `${target.position.y}px`,
                                 position: "absolute",
-                                height: 'calc(100dvh - 7em', overflow: 'auto'
+                                height: "calc(100dvh - 7em)",
+                                overflow: "auto",
                             }}
                         >
                             <Target
