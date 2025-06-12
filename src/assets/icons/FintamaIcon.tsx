dc2e67af98a442bd48f383db1a0e6b91717a005a
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2025-06-12 23:32:47 +0200

    fix build errors and update dependencies

--- src/assets/icons/FintamaIcon.tsx
+++ src/assets/icons/FintamaIcon.tsx
@@ -7,7 +7,7 @@ const FintamaIcon = () => {
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAAAAABXZoBIAAAAUklEQVR4AWMYtGDi6RNQcDUUQ3LvfxiYhalzF0xuCgNWyYUIHqbkEtpIbvMJBYJkcTyu3cWAW3ItDmNPNHV0VAOZZPiT3pJ78UlOPH26iWGwAwBMXl16FVciGwAAAABJRU5ErkJggg=="
             width="2em"
             height="2em"
-            fetchpriority="low"
+            fetchPriority="low"
             alt="Fintama logo"
             data-csiid="mvikZ9WZCsW69u8P7OHV0AE_2"
             data-atf="1"
